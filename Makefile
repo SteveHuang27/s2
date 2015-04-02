018b2aabbeb19c3cb6da0cc347203729d438ffcd
Steve Huang <stevehuang27@gmail.com>
2015-04-02 17:39:21 +0800

    s2 kick off

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -g -o app main.c
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void)
+{
+  	int i;
+	short s1[]={0x1234,0x6578};
+	char str1[80]="Hello, sthis is a book.";
+	char *str2="That is a pen.";
+	char *str3="That is a girl.";
+	i=10;
+	printf("Hello,World! num=%d\n",i);
+}
